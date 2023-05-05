0370ec9579cef9db7b3a3d144082898d9f48323b
smly <smlyjpark@gmail.com>
2023-05-05 12:04:28 -0700

    First commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var likeButton = document.getElementById('like-button');
+var dislikeButton = document.getElementById('dislike-button');
+
+likeButton.addEventListener('click', function() {
+	alert('Thanks for your feedback!');
+});
+
+dislikeButton.addEventListener('click', function() {
+	alert('Sorry to hear that! We will try to improve.');
+});
